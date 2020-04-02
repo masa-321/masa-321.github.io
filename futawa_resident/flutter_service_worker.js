9cefd120c55e22d50eab84fb414ab4d6eab46a5c
masa-321 <ma.ly321.luna.notte@gmail.com>
2020-04-02 11:47:55 +0900

    5th commit

--- futawa_resident/flutter_service_worker.js
+++ futawa_resident/flutter_service_worker.js
@@ -1,8 +1,8 @@
 'use strict';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "index.html": "5cfac702fb8b8aced9ff400b6f440261",
-"main.dart.js": "8b97782f041686a011e5ae939e29a60e",
+  "index.html": "3d1c88eb249f864d7ad0546d3533ef34",
+"main.dart.js": "96b053a40c76ff46c7ed2daddfc727bf",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
